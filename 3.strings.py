f29ee75939c97ed12f07cf60a036445c7b001d12
mcheruvonath@gmail.com <mcheruvonath@gmail.com>
2023-12-31 17:30:40 +0530

    second_commit

---
+++ 3.strings.py
@@ -0,0 +1,24 @@
+text="ice cream"
+print(text)
+print(text[0])
+print(text[1])
+print(text[0:3])
+print(text[4:9])
+print(text[4:])
+print(text[:3])
+text="hello"
+text='hello'
+text="let's learn python"
+text='hello "world"'
+print(text)
+address=''' cheruvonath
+maniyur
+payyoli
+kozhikode'''
+print(address)
+s1="hello"
+s2="world"
+print(s1+s2)
+s="total state in india#"
+num=28
+print(s+str(num))
---
+++ 4.Lists.py
@@ -0,0 +1,26 @@
+item1="brand"
+item2="pasta"
+item3="fruits"
+items=["bread","pasta","fruits","veggies"]
+print(items)
+print(items[0])
+print(items[2])
+items[0]='chips'
+print(items)
+print(items[0:2])
+print(items[-1])
+items=["bred","pasta","fruits","veggies"]
+print(items)
+items.append("butter")
+print(items)
+items=["bred","pasta","fruits","veggies"]
+print(items)
+items.insert(1,'butter')
+print(items)
+food=["bred","pasta","fruit"]
+bathroom=["shampoo","soap"]
+items=food+bathroom
+print(items)
+print(len(items))
+print( "bred" in items)
+print("soda" in items)
